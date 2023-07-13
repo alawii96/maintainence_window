eb88e6698d1b27b7ce92ab829888eebeb2bc0454
Ali AlQattan <exidiful@gmail.com>
2023-07-13 04:13:23 +0300

    temp

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,28 @@ async fn main() -> Result<(), rds::Error> {
 
     let req1 = client.describe_pending_maintenance_actions().send().await?;
 
+    req1.pending_maintenance_actions()
+        .unwrap()
+        .iter()
+        .for_each(|action| {
+            println!(
+                "Action: {:?}:{:?}",
+                action.resource_identifier().unwrap().to_owned(),
+                action
+                    .pending_maintenance_action_details()
+                    .unwrap()
+                    .iter()
+                    .map(|detail| {
+                        format!(
+                            "{:?}:{:?}\n",
+                            detail.action().unwrap(),
+                            detail.auto_applied_after_date().unwrap()
+                        )
+                    })
+                    .collect::<Vec<String>>()
+                    .join(",")
+            );
+        });
     println!("{:#?}", req1.pending_maintenance_actions());
 
     Ok(())
