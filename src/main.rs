4ba39f3e1b8b1ae704c92665e931cd3abca58dd5
Ali AlQattan <exidiful@gmail.com>
2023-07-15 07:11:48 +0300

    please work

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,42 @@
 use aws_sdk_rds as rds;
 
+#[derive(Debug)]
+struct MaintainenceWindow {
+    resource_identifier: String,
+    action: String,
+    auto_applied_after_date: String,
+}
+
 #[::tokio::main]
 async fn main() -> Result<(), rds::Error> {
     let config = ::aws_config::load_from_env().await;
     let client = rds::Client::new(&config);
 
     let req1 = client.describe_pending_maintenance_actions().send().await?;
-
-    req1.pending_maintenance_actions()
+    let list_of_maintainenace_window: Vec<MaintainenceWindow> = req1
+        .pending_maintenance_actions()
         .unwrap()
         .iter()
-        .for_each(|action| {
-            println!(
-                "Action: {:?}:{:?}",
-                action.resource_identifier().unwrap().to_owned(),
-                action
-                    .pending_maintenance_action_details()
-                    .unwrap()
-                    .iter()
-                    .map(|detail| {
-                        format!(
-                            "{:?}:{:?}\n",
-                            detail.action().unwrap(),
-                            detail.auto_applied_after_date().unwrap()
-                        )
-                    })
-                    .collect::<Vec<String>>()
-                    .join(",")
-            );
-        });
-    println!("{:#?}", req1.pending_maintenance_actions());
+        .map(|action| MaintainenceWindow {
+            resource_identifier: action.resource_identifier().unwrap().to_owned(),
+            action: action
+                .pending_maintenance_action_details()
+                .unwrap()
+                .iter()
+                .map(|detail| format!("{:?}", detail.action().unwrap()))
+                .collect::<Vec<String>>()
+                .join(","),
+            auto_applied_after_date: action
+                .pending_maintenance_action_details()
+                .unwrap()
+                .iter()
+                .map(|detail| format!("{:?}", detail.auto_applied_after_date().unwrap()))
+                .collect::<Vec<String>>()
+                .join(","),
+        })
+        .collect();
+
+    println!("{:#?}", list_of_maintainenace_window);
 
     Ok(())
 }
