092c4b545ca2311ccde9b347e112d2fcfeefefe1
Ali AlQattan <exidiful@gmail.com>
2023-07-15 07:24:45 +0300

    looks cooler

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use aws_sdk_rds as rds;
+use prettytable::{Cell, Row, Table};
 
 #[derive(Debug)]
 struct MaintainenceWindow {
@@ -36,7 +37,33 @@ async fn main() -> Result<(), rds::Error> {
         })
         .collect();
 
-    println!("{:#?}", list_of_maintainenace_window);
+    if list_of_maintainenace_window.len() == 0 {
+        println!("No pending maintenance actions found");
+        return Ok(());
+    }
+
+    let mut table = Table::new();
+    table.add_row(Row::new(vec![
+        Cell::new("Actions Count :"),
+        Cell::new(""),
+        Cell::new(&list_of_maintainenace_window.len().to_string()).style_spec("bFg"),
+    ]));
+
+    table.add_row(Row::new(vec![
+        Cell::new("Resource Identifier"),
+        Cell::new("Action"),
+        Cell::new("Auto Applied After Date"),
+    ]));
+
+    for window in list_of_maintainenace_window.iter() {
+        table.add_row(Row::new(vec![
+            Cell::new(&window.resource_identifier),
+            Cell::new(&window.action),
+            Cell::new(&window.auto_applied_after_date),
+        ]));
+    }
+
+    table.printstd();
 
     Ok(())
 }
